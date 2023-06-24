80b03190252a910b0a8e116f58d65d4205d441b5
Sarthak Zalte <sarthakzalte@gmail.com>
2023-06-24 12:11:30 +0530

    Build Jumbotron section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
+      <Jumbotron />
     </div>
   );
 }
