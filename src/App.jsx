9f25e95407da5bdee89d8af112b2d2dcad6f5e6b
Sarthak Zalte <sarthakzalte@gmail.com>
2023-06-24 12:48:46 +0530

    added soundSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
-
+import SoundSection from "./components/SoundSection";
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Nav />
       <Jumbotron />
+      <SoundSection />
     </div>
   );
 }
---
+++ src/components/SoundSection.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function SoundSection() {
+  return (
+    <div className="sound-section wrapper">
+      <div className="body">
+        <div className="sound-section-content content">
+          <h2>New Sound System</h2>
+          <p className="text">Feel the base.</p>
+          <span className="desciption">
+            From $41.62/mo. for 24 mo. or $999 before trade-in
+          </span>
+          <ul className="links">
+            <li>
+                <button className="button">Buy</button>
+            </li>
+            <li>
+                <a className="Link">Learn more</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
