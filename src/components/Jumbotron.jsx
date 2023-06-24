a4d93d09d5c7e153fc6691b30e066e8963435886
Sarthak Zalte <sarthakzalte@gmail.com>
2023-06-24 12:57:46 +0530

    Added the function to scroll down the next section on click of button.

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,26 @@ import Iphone from '../assets/images/iphone-14.jpg';
 import HoldingIphone from '../assets/images/iphone-hand.png'
 
 export default function Jumbotron() {
+
+
+    //function to scroll down to sound section
+    const handleLearnMore = () => {
+
+        //taking the element to which we have to scroll
+        const element = document.querySelector(".sound-section");
+        
+        //scroll to function helps in doing that which accepts couple of paramerters like postion (top/bottom), left for left scrolling and animation effect and many more
+        window.scrollTo({
+
+            //here ? is added as chaining operator so that if the defined elemt is not there plz don;t execute the function this will prevent the error.
+            top: element?.getBoundingClientRect().top,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
+
+
   return (
     <div className='jumbotron-section wrapper'>
         <h2 className='title'>New</h2>
@@ -16,7 +36,7 @@ export default function Jumbotron() {
                 <button className='button'>Buy</button>
             </li>
             <li>
-                <a className='link'>Learn more</a>
+                <a className='link' onClick={handleLearnMore}>Learn more</a>
             </li>
         </ul> 
         <img className='iphone-img' src={HoldingIphone} alt='iPhone' />
