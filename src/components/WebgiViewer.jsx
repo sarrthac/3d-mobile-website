cd0b1e97f40a1fc403d290a8d725549cdc72a1fd
Sarrthac <sarthakzalte@gmail.com>
2023-06-27 13:18:42 +0530

    Registered the Scroll Trigger Plugin

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -15,6 +15,8 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect } from "react";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function WebgiViewer() {
   const canvasRef = useRef(null);
 
@@ -28,13 +30,14 @@ export default function WebgiViewer() {
     // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin);
 
+
     const camera = viewer.scene.activeCamera;
     const position = camera.position;
     const target = camera.target;
 
 
     // Add plugins individually.
-     await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(GBufferPlugin)
     await viewer.addPlugin(new ProgressivePlugin(32))
     await viewer.addPlugin(new TonemapPlugin(true))
     await viewer.addPlugin(GammaCorrectionPlugin)
@@ -48,20 +51,20 @@ export default function WebgiViewer() {
 
     await manager.addFromPath("scene-black.glb");
 
-    viewer.getPlugin(TonemapPlugin).config.clipBackground  = true;
+    viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
 
-    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false});
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
 
     //AFTER RELOAD THE POSITION OF PAGE IS ON TOP
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
 
     let needsUpdate = true;
 
     viewer.addEventListener("preFrame", () => {
-        if(needsUpdate){
+      if (needsUpdate) {
         camera.positionTargetUpdated(true);
         needsUpdate = false;
-        }
+      }
     });
 
   }, []);
